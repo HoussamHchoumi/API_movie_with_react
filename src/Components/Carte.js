191e6e7526bc89e64626d5240225c5f146ef0a0b
HOUSSAM EDDINE <houssamhchoumi@gmail.com>
2023-03-18 22:17:32 +0100

    finaly commit

--- src/Components/Carte.js
+++ src/Components/Carte.js
@@ -3,20 +3,21 @@ import react from "react";
 
 const Carte=(movie)=>{
     console.log(movie.info);
+    let img_path="https://image.tmdb.org/t/p/w500";
     return(
         <>
             <div className="movie">
-                <img src="images/poster.jpg" className="poster">
+                <img src={img_path+movie.info.poster_path} className="poster">
 
                 </img>
                 <div className="movie-details">
                     <div className="box">
-                        <h4 className="title">Movie Titre</h4>
-                        <p className="rating">9.7</p>
+                        <h4 className="title">{movie.info.title}</h4>
+                        <p className="rating">{movie.info.vote_average}</p>
                     </div>
                     <div className="overview">
-                        <h1>vue générale</h1>
-                        After est une série américaine romantique composée de quatre films, adaptés des romans éponymes de la romancière Anna Todd.
+                        <h1>overview</h1>
+                        {movie.info.overview}
                     </div>
                 </div>
             </div>
--- src/Components/Main.js
+++ src/Components/Main.js
@@ -4,33 +4,63 @@ import Carte from "./Carte";
 let API_key="&api_key=db95773a7fb212ba790d71f6adac0e7e";
 let base_url="https://api.themoviedb.org/3";
 let url=base_url+"/discover/movie?sort_by=popularity.desc"+API_key;
+let arr=["Populaire","Theatre","Enfants","Drama","Comedie"];
 const Main=()=>{
     const [movieData,setData]=useState([]);
     const [url_set,setUrl]=useState(url);
-
+    const [search, setSearch]=useState();
     useEffect(()=>{
         fetch(url_set).then(res=>res.json()).then(data=>{
             //console.log(data.results);
             setData(data.results);
         });
     },[url_set]
-
     )
+    const getData=(movieType)=>{
+        if(movieType=="Populaire"){
+            url=base_url+"/discover/movie?sort_by=popularity.desc"+API_key;
+        }
+        if(movieType=="Theatre"){
+            url=base_url+"/discover/movie?primary_release_date.gte=2014-09-15&primary_release_date.lte=2014-10-22"+API_key;
+        }
+        if(movieType=="Enfants"){
+            url=base_url+"/discover/movie?certification_country=US&certification.lte=G&sort_by=popularity.desc"+API_key;
+        }
+        if(movieType=="Drama"){
+            url=base_url+"/discover/movie?with_genres=18&primary_release_year=2014"+API_key;
+        }
+        if(movieType=="Comedie"){
+            url=base_url+"/discover/movie?with_genres=35&with_cast=23659&sort_by=revenue.desc"+API_key;
+        }
+        setUrl(url);
+    }
+    const searchMovie=(evt)=>{
+        if(evt.key=="Enter")
+        {
+            //console.log("salut")
+            url=base_url+"/search/movie?api_key=db95773a7fb212ba790d71f6adac0e7e&query="+search;
+            setUrl(url);
+            setSearch(" ");
+        }
+    }
+    
     return(
         <>
             <div className="header">
                 <nav>
                     <ul>
-                        <li><a href="#">Populaire</a></li>
-                        <li><a href="#">Theatre</a></li>
-                        <li><a href="#">enfants</a></li>
-                        <li><a href="#">Drama</a></li>
-                        <li><a href="#">Comedie</a></li>
+                        {
+                            arr.map((value)=>{
+                                return(
+                                    <li><a href="#" name={value} onClick={(e)=>{getData(e.target.name)}} >{value}</a></li>
+                                )
+                            })
+                        }
                     </ul>
                 </nav>
                 <form>
                     <div className="search-btn">
-                        <input type="text" placeholder="entrer votre movie" className="inputText"></input>
+                        <input type="text" placeholder="entrer votre movie" className="inputText" onChange={(e)=>{setSearch(e.target.value)}} value={search} onKeyPress={searchMovie} ></input>
                         <button><i class="bi bi-search"></i></button>
                     </div>
                 </form>
