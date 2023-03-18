c3afe1bdd230ac2c50783f477a295168a48c5a20
HOUSSAM EDDINE <houssamhchoumi@gmail.com>
2023-03-18 18:38:02 +0100

    suite commit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import Main from './Components/Main';
 import './Components/Style.css';
+import Carte from './Components/Carte';
 
 function App() {
   return (
